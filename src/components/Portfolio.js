624ccf437c55486224cc617733dcab572f6c29dd
MarkNou <mark.nou95@gmail.com>
2023-06-21 17:11:05 -0700

    updated hero pic and projects

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import tictactoe from '../assets/portfolio/tictactoe.jpg'
-import piglatin from '../assets/portfolio/piglatin.png'
+import concertgo from '../assets/portfolio/concertgo.png'
+import hightension from '../assets/portfolio/hightension.png'
 import treasurehunt from '../assets/portfolio/treasurehunt.png'
 
 const Portfolio = () => {
@@ -8,24 +8,27 @@ const Portfolio = () => {
     const portfolios = [
         {
             id: 1,
-            src: tictactoe,
-            url1: 'https://github.com/MarkNou/tic-tac-toe-mark-and-apolonio',
-            url2: 'https://github.com/MarkNou/tic-tac-toe-mark-and-apolonio',
-            title: 'Tic Tac Toe'
+            src: concertgo,
+            url1: 'https://concertgo.onrender.com/',
+            url2: 'https://github.com/Wham-Devs/concert-go-frontend',
+            title: 'Concert Go',
+            description: 'Concert Go is a fullstack application for concert venues. Written in React, Ruby on Rails, and PostgreSQL.'
         },
         {
             id: 2,
-            src: piglatin,
-            url1:'https://lucent-lolly-4b6d3e.netlify.app',
-            url2: 'https://github.com/MarkNou/pig-latin-the-pig-vinci-code.git',
-            title: 'Pig Latin'
+            src: hightension,
+            url1:'https://github.com/learn-academy-2023-bravo/text-based-game-high-tension-2',
+            url2: 'https://github.com/learn-academy-2023-bravo/text-based-game-high-tension-2',
+            title: 'High Tension',
+            description: 'High Tension is a text based game that allows users to choose their own adventure. Written in Ruby.'
         },
         {
             id: 3,
             src: treasurehunt,
             url1: 'https://celadon-meringue-36b01c.netlify.app/',
             url2: 'https://github.com/MarkNou/treasure-hunt-MarkNou',
-            title: 'Treasure Hunt'
+            title: 'Treasure Hunt',
+            description: 'Treasure Hunt is a game that allows users to search for a treasure among bombs. Written in React.'
         }
         
     ]
@@ -41,9 +44,10 @@ const Portfolio = () => {
             
             <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0 '>
             {
-                portfolios.map(({id, src, url1, url2, title}) => (
+                portfolios.map(({id, src, url1, url2, title, description}) => (
                     <div key={id} className='shadow-md shadow-purple-500 rounded-lg '>
-                        <p className='text-center'>{title}</p>
+                        <p className='text-center'>{title}</p><br/>
+                        <p className='text-center'>{description}</p>
                     <img src={src} alt='' className='rounded-md duration-200 hover:scale-105' />
                     <div className='flex items-center justify-center'> 
                     <button onClick={() => window.open(url1, '_blank')} className='w-1/2 px-6 py-4 m-4 duration-200 hover:scale-105' >Demo</button>
